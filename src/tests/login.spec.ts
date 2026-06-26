6bab4e061e84b92004f6dabb7cedfde9656dd4bd
Toan Can <toancan3695@gmail.com>
2026-06-26 14:37:06 +0700

    add testcase loginPage and update yml

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -1,6 +1,9 @@
 import { expect } from '@playwright/test';
 import { test } from 'fixtures/fixtures';
+import { ENV } from '../config/env';
 
-test('open homepage', async ({ homePage, page }) => {
-    await homePage.gotoHomePage();
+test('Login success', async ({ loginPage, page }) => {
+
+    await loginPage.openLoginPage();
+    await loginPage.login(ENV.USER_NAME, ENV.USER_PASSWORD);
 })
